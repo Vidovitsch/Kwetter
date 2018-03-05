aba0a82d0a390f4a1d2c1a57acba97ac405f8f81
Vidovitsch <david.de.bekker@gmail.com>
2018-03-05 11:07:02 +0100

    Search, Timeline and Trend methods added

--- src/main/java/DAO/Mock/ProfileDaoMock.java
+++ src/main/java/DAO/Mock/ProfileDaoMock.java
@@ -12,7 +12,7 @@ public class ProfileDaoMock implements IProfileDao {
     private Collection<Profile> mockProfiles;
 
     public ProfileDaoMock(Collection<User> users) {
-        this.mockProfiles = createmockProfiles(users);
+        this.mockProfiles = createMockProfiles(users);
     }
 
     public Collection<Profile> findAll() {
@@ -71,7 +71,7 @@ public class ProfileDaoMock implements IProfileDao {
         return mockProfiles.remove(profile);
     }
 
-    private ArrayList<Profile> createmockProfiles(Collection<User> users) {
+    private ArrayList<Profile> createMockProfiles(Collection<User> users) {
         ArrayList<Profile> profiles = new ArrayList<Profile>();
         for (User user : users) {
             Profile dummyProfile = new Profile(user.getUsername() + " Test");
