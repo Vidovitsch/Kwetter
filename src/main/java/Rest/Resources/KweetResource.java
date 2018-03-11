c2471eb42de395d6b09a34853c519ecc26238e19
Michiel van Eijkeren <m.vaneijkeren@gmail.com>
2018-03-11 22:47:39 +0100

    Code update

--- src/main/java/Rest/Resources/KweetResource.java
+++ src/main/java/Rest/Resources/KweetResource.java
@@ -1,4 +1,57 @@
 package Rest.Resources;
 
+import DAO.Mock.KweetDaoMock;
+import DAO.Mock.UserDaoMock;
+import DaoInterfaces.IKweetDao;
+import DaoInterfaces.IUserDao;
+import Service.ProfileDataService;
+import Service.TimelineService;
+import ViewModels.ProfileDataView;
+import ViewModels.TimelineItem;
+import ViewModels.UserTotalsView;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import java.util.Set;
+
+
+@Path("kweet")
+@Api(value = "kweet resource")
 public class KweetResource {
+
+    @Context
+    private UriInfo context;
+
+    public KweetResource(){
+    }
+
+    @GET
+    @Path("/mostrecent/byuserid/{userid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Retrieve a users 10 most recent kweets", notes = "User id has to be valid and kweets have to be available")
+    public Set<TimelineItem> getMostRecentKweetsByID(@PathParam("userid") long userid) {
+        TimelineService timelineService = new TimelineService();
+        Set<TimelineItem> mostRecentKweets = timelineService.TenMostRecentKweets(userid);
+        return mostRecentKweets;
+    }
+
+    @GET
+    @Path("/timeline/byuserid/{userid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Retrieve a users 10 most recent kweets", notes = "User id has to be valid and kweets have to be available")
+    public Set<TimelineItem> getTimelineByUserID(@PathParam("userid") long userid) {
+        TimelineService timelineService = new TimelineService();
+        Set<TimelineItem> mostRecentKweets = timelineService.GenerateTimeLine(userid);
+        return mostRecentKweets;
+    }
+
+
+
 }
--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -35,27 +35,37 @@ public class UserService {
         return false;
     }
 
+    public List<OtherUserView> getFollowers(String username) {
+        User user = userDao.findByUsername(username);
+        return generateOtherUserViews(user.getFollowers());
+    }
+
+    public List<OtherUserView> getFollowers(long userid) {
+        User user = userDao.findById(userid);
+        return generateOtherUserViews(user.getFollowers());
+    }
+
     public List<OtherUserView> getFollowing(String username) {
         User user = userDao.findByUsername(username);
-        return generateFollowingList(user);
+        return generateOtherUserViews(user.getFollowing());
     }
 
     public List<OtherUserView> getFollowing(long userid) {
         User user = userDao.findById(userid);
-        return generateFollowingList(user);
+        return generateOtherUserViews(user.getFollowing());
     }
 
-    private List<OtherUserView> generateFollowingList(User user){
-        ArrayList<OtherUserView> following = new ArrayList<>();
-        for (User u : user.getFollowing()) {
+    private List<OtherUserView> generateOtherUserViews(List<User> users){
+        ArrayList<OtherUserView> OtherUserViews = new ArrayList<>();
+        for (User u : users) {
             String image = "";
             try {
                 image = u.getProfile().getImage();
             } catch (Exception e) {
             }
-            following.add(new OtherUserView(u.getUsername(), image));
+            OtherUserViews.add(new OtherUserView(u.getUsername(), image));
         }
-        return following;
+        return OtherUserViews;
     }
 }
 
