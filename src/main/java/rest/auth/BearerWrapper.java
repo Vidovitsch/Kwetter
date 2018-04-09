154680aaa6ea5b5adaf914b57a2c61d8a35e7c0c
Michiel van Eijkeren <m.vaneijkeren@gmail.com>
2018-04-09 12:27:12 +0200

    Add call for token validation

---
+++ src/main/java/rest/auth/BearerWrapper.java
@@ -0,0 +1,13 @@
+package rest.auth;
+
+public class BearerWrapper {
+
+    public String Bearer;
+
+    public BearerWrapper(String bearer) {
+        Bearer = bearer;
+    }
+
+    public BearerWrapper() {
+    }
+}
