5235f021b806bd907c988d34f55809dbe05f1cf6
Vidovitsch <david.de.bekker@gmail.com>
2018-05-07 12:12:40 +0200

    Websocket support added

--- src/main/java/websocket/KweetEndpoint.java
+++ src/main/java/websocket/KweetEndpoint.java
@@ -1,19 +1,20 @@
 package websocket;
 
-
+import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-@ServerEndpoint("/kweet")
+@ApplicationScoped
+@ServerEndpoint(value = "/websocket/kweet")
 public class KweetEndpoint {
 
     private static Set<Session> clients = Collections.synchronizedSet(new HashSet<>());
 
     @OnMessage
-    public String onMessage(String message) {
+    public String onMessage(String message, Session session) {
         return null;
     }
 
